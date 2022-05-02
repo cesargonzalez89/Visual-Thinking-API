6caf9c9b15723345a74b22c3d18c3c1053556713
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-01 19:25:46 -0500

    Se agrega la prueba para el metodo getMailStudentsCertificated

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -6,4 +6,11 @@ describe("Tests para StudentService", () => {
         const studentsService = StudentService.getAllStudents(students);
         expect(studentsService.length).toBe(1);
     });
+
+    test("Requerimiento 2: Obtener los emails de todos los estudiantes que tengan certificación", () => {
+        const students = [{name: "Warren", email: "Todd@visualpartnership.xyz", haveCertification: true}, {name: "Lucinda", email: "Sexton@visualpartnership.xyz",haveCertification: true}, {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
+        const studentsService = StudentService.getMailStudentsCertificated(students);
+        expect(studentsService.length).toBe(2);
+    });
+
 });
