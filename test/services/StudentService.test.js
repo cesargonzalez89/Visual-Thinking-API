e6824ef1a38ae8c41630e42266d4a9f7bfea14b8
Cesar Gonzalez <jugador409@hotmail.com>
2022-05-01 19:49:00 -0500

    Se agregan pruebas para getStudentsWithCredits

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,5 +1,5 @@
 const StudentService = require("./../../lib/services/StudentService");
-
+const Reader = require('./../../lib/utils/reader');
 describe("Tests para StudentService", () => {
     test("Requerimiento 1: Obtener la cantidad de estudiantes", () => {
         const students = [{name: "Warren"}];
@@ -7,10 +7,23 @@ describe("Tests para StudentService", () => {
         expect(studentsService.length).toBe(1);
     });
 
-    test("Requerimiento 2: Obtener los emails de todos los estudiantes que tengan certificación", () => {
+    test("Requerimiento 2: Obtener los emails de todos los estudiantes que tengan certificación.", () => {
         const students = [{name: "Warren", email: "Todd@visualpartnership.xyz", haveCertification: true}, {name: "Lucinda", email: "Sexton@visualpartnership.xyz",haveCertification: true}, {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
         const studentsService = StudentService.getMailStudentsCertificated(students);
         expect(studentsService.length).toBe(2);
+    });
+
+    test("Requerimiento 3: Obtener los emails de todos los estudiantes que tengan certificación.", () => {
+        const students = [{name: "Warren", email: "Todd@visualpartnership.xyz", haveCertification: true}, {name: "Lucinda", email: "Sexton@visualpartnership.xyz",haveCertification: true}, {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
+        const studentsService = StudentService.getMailStudentsCertificated(students);
+        expect(studentsService).toStrictEqual(["Todd@visualpartnership.xyz", "Sexton@visualpartnership.xyz"]);
+    });
+
+    test("Requerimiento 4: consultar todos los estudiantes que tengan credits mayor a 500.", () => {
+        //const students = [{name: "Warren", email: "Todd@visualpartnership.xyz", haveCertification: true}, {name: "Lucinda", email: "Sexton@visualpartnership.xyz",haveCertification: true}, {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
+        const students = Reader.readJsonFile("StudentService.test.json");
+        const studentsService = StudentService.getStudentsWithCredits(students);
+        expect(studentsService.length).toStrictEqual(0);
     });
 
 });
